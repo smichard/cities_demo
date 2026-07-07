e856c167ec19f8c04c4e9cfd95b0eec2b5d1b3be
Stephan Michard <github@michard.io>
2026-07-07 13:59:25 +0200

    demo resteored

--- update_script.sh
+++ update_script.sh
@@ -4,7 +4,6 @@ if [ -f ./website/index_2.html ]; then
         mv website/index_2.html website/index.html
         mv website/index_3.html website/index_2.html
         mv website/index_4.html website/index_3.html
-        mv website/index_5.html website/index_4.html
-        mv website/index_tmp.html website/index_5.html
+        mv website/index_tmp.html website/index_4.html
 		echo changes performed
 fi
